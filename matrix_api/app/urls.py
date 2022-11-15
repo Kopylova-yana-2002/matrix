c60105546c1993044028e8b2c17217f6237b06bc
yana <yana_22_20022mail.ru>
2022-11-15 22:17:04 +0300

    создана документация

--- matrix_api/app/urls.py
+++ matrix_api/app/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 from . import views
 
 urlpatterns = [
-    path('inverse/', views.MatrixInverterView.as_view())
+    path('inverse/', views.MatrixInverterView.as_view()),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(
+            url_name="schema"
+        ),
+        name="swagger-ui",
+    ),
 ]
--- matrix_api/app/views.py
+++ matrix_api/app/views.py
@@ -5,48 +5,79 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .Models import MatrixInverter
 from .serializers import MatrixSerializer
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
 
 
 
 # Create your views here.
 class MatrixInverterView(APIView):
 
-
-
+    @extend_schema(
+        responses={400: str, 200: MatrixSerializer},
+        methods=["GET"]
+    )
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(name='size', location=OpenApiParameter.QUERY,
+                             description='matrix size', required=True, type=int),
+            OpenApiParameter(name='matrix', location=OpenApiParameter.QUERY,
+                             description='matrix', required=True,
+                             examples=[OpenApiExample('2x2 matrix', value='1_2-3_4'),
+                                       OpenApiExample('3x3 matrix', value='1_2_3-4_5_6-9_4_2')])
+        ],
+        description='Calculates inverse matrix',
+        examples=[OpenApiExample('2x2 matrix',
+                                 value=MatrixSerializer(
+                                     instance=MatrixInverter(2, [[1, 2], [2, 1]], [[-0.33, 0.66], [0.66, -0.33]])).data,
+                                 response_only=True,
+                                 status_codes=[200]
+                                 ),
+                  OpenApiExample('No size',
+                                 value='Размер не указан',
+                                 response_only=True,
+                                 status_codes=[400]
+                                 ),
+                  OpenApiExample('Invalid matrix',
+                                 value='Матрица неверного размера',
+                                 response_only=True,
+                                 status_codes=[400]
+                                 )
+                                ]
+    )
     def get(self, request):
         size = request.GET.get('size', None)
 
         if size is None:
-            return HttpResponse('Размер не указан')
+            return Response('Размер не указан', 400)
 
         try:
             size = int(size)
         except:
-            return HttpResponse('Размер не число')
+            return Response('Размер не число', 400)
 
         if size <= 0:
-            return HttpResponse('Размер меньше или равен 0')
+            return Response('Размер меньше или равен 0', 400)
 
         matrix = request.GET.get('matrix', None)
 
         if matrix is None:
-            return HttpResponse('Матрица не указана')
+            return Response('Матрица не указана', 400)
 
         matrix = matrix.split('-')
 
         new_matrix = []
 
         if len(matrix) != size:
-            return HttpResponse('Матрица неверного размера')
+            return Response('Матрица неверного размера', 400)
 
         for row in matrix:
             try:
                 row = list(map(float, row.split('_')))
             except:
-                return HttpResponse('В матрице содержатся недопустимые значения')
+                return Response('В матрице содержатся недопустимые значения', 400)
 
             if len(row) != size:
-                return HttpResponse('Реверный размер строки в матрице')
+                return Response('Реверный размер строки в матрице', 400)
 
             new_matrix.append(row)
 
@@ -55,7 +86,7 @@ class MatrixInverterView(APIView):
         try:
             inverse_matrix = np.linalg.inv(new_matrix)
         except np.linalg.LinAlgError as e:
-            return HttpResponse("Определитель матрицы равен нулю")
+            return Response("Определитель матрицы равен нулю", 400)
 
         for row in inverse_matrix:
             for i in range(len(row)):
@@ -63,5 +94,4 @@ class MatrixInverterView(APIView):
 
         matrixInverter = MatrixInverter(size, new_matrix, inverse_matrix)
         serializer_for_request = MatrixSerializer(instance=matrixInverter)
-        return Response(serializer_for_request.data)
-
+        return Response(serializer_for_request.data, 200)
